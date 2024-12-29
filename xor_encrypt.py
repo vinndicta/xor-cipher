b8eb070870975aceeb19aa44878d967f693df0a2
Aymen <aymenbasilicum@gmail.com>
2024-12-29 14:10:39 +0100

    Update xor_encrypt.py

--- xor_encrypt.py
+++ xor_encrypt.py
@@ -18,21 +18,21 @@ for m in msg:
 if len(key_b) > len(msg_b): # if the key is longer than the message...
         key_b = key_b[:len(msg_b)] # ...we truncate the key to match the latter's length
 else: # we make sure the key is the same length as the message
-    while len(key_b) != len(msg_b):
+    while len(key_b) < len(msg_b):
         for i in range(len(key_b)):
             key_b += key_b[i]
 
 # i know that python has a built-in xor operation method, i wanted to use my own
 def xor(k_bit, m_bit):
     if k_bit == m_bit:
-        return 0 # 0 ⊕ 0 = 0     |   1 ⊕ 1 = 0
+        return 0 # 0 ⊕ 0 = 0    |   1 ⊕ 1 = 0
     else:
         return 1  # 1 ⊕ 0 = 1   |    0 ⊕ 1 = 1
 
 def x_cipher(key_b, msg_b):
     cipher = ''
-    for bit in range(len(msg_b)): # we apply the xor bit by bit
-        cipher += str(xor(int(key_b[bit]), int(msg_b[bit]))) 
+    for bit in range(len(msg_b)):
+        cipher += str(xor(int(key_b[bit]), int(msg_b[bit])))
     return cipher
 
 print(f"\nCipher : {x_cipher(key_b, msg_b)}\n")
