35f8ad487ee2b04defe7daee9d83b1a3fb5e779a
Aymen <aymenbasilicum@gmail.com>
2025-01-31 00:38:09 +0100

    Update xor_encrypt.py

--- xor_encrypt.py
+++ xor_encrypt.py
@@ -1,26 +1,8 @@
-key = input("\nKey: ")
-msg = input("Message: ")
-
-k_bin = input("Is your key in binary format? (y/n): ")
-
-if k_bin == 'n':
-    key_b = '' # '_b' for binary
-    for k in key :
-        key_b += format(ord(k), '08b') # the specifier '08b' converts k into an 8-bit binary
-else:
-    key_b = key
-
-# convert the key to a binary representation
-msg_b = ''
-for m in msg:
-    msg_b += format(ord(m), '08b')
-
-if len(key_b) > len(msg_b): # if the key is longer than the message...
-        key_b = key_b[:len(msg_b)] # ...we truncate the key to match the latter's length
-else: # we make sure the key is the same length as the message
-    while len(key_b) < len(msg_b):
-        for i in range(len(key_b)):
-            key_b += key_b[i]
+def convert_to_binary(entry):
+    entry_b = ''
+    for e in entry:
+        entry_b += format(ord(e), '08b')
+    return entry_b
 
 # i know that python has a built-in xor operation method, i wanted to use my own
 def xor(k_bit, m_bit):
@@ -35,4 +17,28 @@ def x_cipher(key_b, msg_b):
         cipher += str(xor(int(key_b[bit]), int(msg_b[bit])))
     return cipher
 
-print(f"\nCipher : {x_cipher(key_b, msg_b)}\n")
+def main():
+    key = input("\nKey: ")
+    msg = input("Message: ")
+
+    k_bin = input("Is your key in binary format? (y/n): ")
+
+    if k_bin == 'n':
+        key_b = convert_to_binary(key)
+    else:
+        key_b = key
+
+    # express the message to a binary representation
+    msg_b = convert_to_binary(msg)
+
+    if len(key_b) > len(msg_b): # if the key is longer than the message...
+            key_b = key_b[:len(msg_b)] # ...we truncate the key to match the latter's length
+    else: # we make sure the key is the same length as the message
+        while len(key_b) < len(msg_b):
+            for i in range(len(key_b)):
+                key_b += key_b[i]
+
+    print(f"\nCipher : {x_cipher(key_b, msg_b)}\n")
+
+if __name__ == "__main__":
+    main()
